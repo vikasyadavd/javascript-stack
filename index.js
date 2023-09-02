b821b30af11d2ff02c358ec2f1b3841b5e2bcf66
vikasyadavd <138962030+vikasyadavd@users.noreply.github.com>
2023-09-02 16:02:53 +0530

    Handled underflow

--- index.js
+++ index.js
@@ -1,46 +1,53 @@
 /**
  * LIFO (Last-In-First-Out) Order
- * 
+ *
  * Use Cases: Stacks are used in a variety of applications,
  *  including function call management in programming languages (the call stack),
  *  expression evaluation, undo/redo functionality in software, and parsing operations (e.g., syntax checking)
- * 
+ *
  * Efficiency: The time complexity of basic stack operations
  *  (push, pop, peek) is typically O(1),
  *  which means they have constant time complexity and are very efficient.
  */
 class Stack {
-    constructor() {
-        this.top = -1;
-        this.items = {};
-    }
+  constructor() {
+    this.top = -1;
+    this.items = {};
+  }
 
-    peek() {
-        return this.items[this.top];
-    }
+  peek() {
+    return this.items[this.top];
+  }
 
-    size() {
-        return this.top + 1;
-    }
+  size() {
+    return this.top + 1;
+  }
 
-    isEmpty() {
-        return this.top === -1;
-    }
+  isEmpty() {
+    return this.top === -1;
+  }
 
-    // Overflow (stacks may have a maximum capacity)
-    // Underflow (try to pop an element from an empty stack)
+  // Overflow (stacks may have a maximum capacity)
+  // Underflow (try to pop an element from an empty stack)
 
-    push(value) {
-        this.top += 1;
-        this.items[this.top] = value;
-    }
+  push(value) {
+    this.top += 1;
+    this.items[this.top] = value;
+  }
 
-    pop() {
-        const valueToPop = this.items[this.top];
-        delete this.items[this.top];
-        this.top -= 1;
-        return valueToPop;
+  pop() {
+    if (this.#underflow()) {
+      return null;
     }
+    const valueToPop = this.items[this.top];
+    delete this.items[this.top];
+    this.top -= 1;
+    return valueToPop;
+  }
+
+  #underflow() {
+    return this.top === -1;
+  }
 }
 
 module.exports = { Stack };
