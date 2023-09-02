796c263b5fdc948f21c0f9baae9105ac6f4778d6
Vikas Yadav <work.vikasyadav@gmail.com>
2023-09-02 12:24:15 +0530

    Stack implementation

--- index.test.js
+++ index.test.js
@@ -8,17 +8,23 @@ describe('Javascript Stack', () => {
   it('Should created Empty', () => {
     expect(stack.top).toBe(-1);
     expect(stack.items).toEqual({});
+    expect(stack.isEmpty()).toBe(true);
+    expect(stack.size()).toBe(0);
   });
 
   it('Can push to the top', () => {
     stack.push(20);
     expect(stack.top).toBe(0);
     expect(stack.peek()).toBe(20);
+    expect(stack.isEmpty()).toBe(false);
+    expect(stack.size()).toBe(1);
   });
 
   it('Can pop value from the top', () => {
     expect(stack.pop()).toBe(20);
     expect(stack.items).toEqual({});
     expect(stack.top).toBe(-1);
+    expect(stack.isEmpty()).toBe(true);
+    expect(stack.size()).toBe(0);
   });
 });
